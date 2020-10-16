b48daddc588483dedef727066630d72ae49a4141
Camilo Ardila <caardila41@gmail.com>
2020-10-16 12:46:20 -0500

    Recycler funcionando

--- app/src/main/java/com/ard/agrocampo/CreacionProyecto.java
+++ app/src/main/java/com/ard/agrocampo/CreacionProyecto.java
@@ -1,6 +1,7 @@
 package com.ard.agrocampo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -11,27 +12,55 @@ import android.widget.ListView;
 import android.widget.SearchView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CreacionProyecto extends AppCompatActivity {
 
     SearchView srv;
-    RecyclerView lst;
-    ArrayList<String> List;
-    ArrayAdapter adapter;
+    RecyclerView recyclerView;
+    RecyclerAdapater recyclerAdapater;
+
+    List<String> prodiuctos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creacion_proyecto);
+        srv=(SearchView)findViewById(R.id.buscardor);
+        recyclerView=(RecyclerView) findViewById(R.id.lista);
+        recyclerView.setHasFixedSize(true);
+
+        prodiuctos=new ArrayList<>();
+        recyclerAdapater=new RecyclerAdapater(prodiuctos);
+
+
+        // recyclerView.setLayoutManager();
+
+        recyclerView.setAdapter(recyclerAdapater);
+
+        DividerItemDecoration dividerItemDecoration=new DividerItemDecoration(this,DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        prodiuctos.add("Arroz");
+        prodiuctos.add("Papa");
+        prodiuctos.add("Cerdos");
+        prodiuctos.add("Vacas");
+        prodiuctos.add("Pollos");
+        prodiuctos.add("Gallinas");
+        prodiuctos.add("Manzanas");
+        prodiuctos.add("Arroz");
+        prodiuctos.add("Arroz");
+        prodiuctos.add("Arroz");
+        prodiuctos.add("Arroz");
+        prodiuctos.add("Arroz");
+        prodiuctos.add("Arroz");
+        prodiuctos.add("Arroz");
+
+
     }
 
     public  void  volver(View view){
         Intent i=new Intent(this, Inicio.class);
         startActivity(i);
-        srv=(SearchView)findViewById(R.id.buscardor);
-        lst=(RecyclerView) findViewById(R.id.lista);
-
-
-
 
 
     }
--- app/src/main/java/com/ard/agrocampo/RecyclerAdapater.java
+++ app/src/main/java/com/ard/agrocampo/RecyclerAdapater.java
@@ -1,33 +1,74 @@
 package com.ard.agrocampo;
 
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class RecyclerAdapater extends RecyclerView.Adapter<RecyclerAdapater.ViewHolder> {
+private static final String TAG="RegistrarCorreo";
+
+
+    List<String> productos;
+
+    public RecyclerAdapater(List<String> productos) {
+        this.productos = productos;
+    }
 
     @NonNull
     @Override
+
+
+
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        LayoutInflater layoutInflater=LayoutInflater.from(parent.getContext());
+        View view=layoutInflater.inflate(R.layout.productos,parent,false);
+        ViewHolder viewHolder=new ViewHolder(view);
+
+
+
+        return viewHolder;
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        holder.producto.setText(productos.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 20;
+
+        return productos.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        ImageView imageView;
+        TextView producto,tipo;
 
         public ViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            imageView=itemView.findViewById(R.id.imagePro);
+            producto=itemView.findViewById(R.id.tvproducto);
+            tipo=itemView.findViewById(R.id.tvtipo);
+
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+
         }
     }
 }
