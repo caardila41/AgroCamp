793b1e6c1046e8d9bab9aa2f66e60c84711fe1ba
Camilo Ardila <caardila41@gmail.com>
2020-10-16 09:43:53 -0500

    Valores de perfil en el header

--- app/src/main/java/com/ard/agrocampo/CreacionProyecto.java
+++ app/src/main/java/com/ard/agrocampo/CreacionProyecto.java
@@ -1,6 +1,7 @@
 package com.ard.agrocampo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 public class CreacionProyecto extends AppCompatActivity {
 
     SearchView srv;
-    ListView lst;
+    RecyclerView lst;
     ArrayList<String> List;
     ArrayAdapter adapter;
     @Override
@@ -27,36 +28,8 @@ public class CreacionProyecto extends AppCompatActivity {
         Intent i=new Intent(this, Inicio.class);
         startActivity(i);
         srv=(SearchView)findViewById(R.id.buscardor);
-        lst=(ListView)findViewById(R.id.lista);
-        List=new ArrayList<String>();
+        lst=(RecyclerView) findViewById(R.id.lista);
 
-        List.add("Papa");
-        List.add("Tomate");
-        List.add("Bovinos");
-        List.add("Equinos");
-        List.add("Porcionos");
-        List.add("Gallinas");
-        List.add("Pollos");
-        List.add("Bovinos");
-        List.add("Cabras");
-        List.add("Zanahoria");
-
-
-        adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,lst);
-        lst.setAdapter(adapter);
-        srv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-            adapter.getFilter().filter(s);
-                return false;
-            }
-        });
 
 
 
